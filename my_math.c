29ae0f631a51e6b72b8504030c7a9c1a1781c6d1
arielag1998@gmail.com <ariela@ubuntu.myguest.virtualbox.org>
2024-02-10 01:11:58 +0200

    maybe works now

--- my_math.c
+++ my_math.c
@@ -3,8 +3,16 @@
 #define SIZE 10 // size of matrix board
 #endif
 
+#define NO_PATH -1
+#define INF 99999 // Just an example, use an appropriate value based on your application
+
 int shortest_pathhelp(int x, int y, int graph[][SIZE], int graph2[SIZE], int soFer)
 {
+    if (x < 0 || x >= SIZE || y < 0 || y >= SIZE)
+    {
+        return NO_PATH; // Input coordinates out of range
+    }
+
     if (x == y)
     {
         return soFer;
@@ -12,45 +20,31 @@ int shortest_pathhelp(int x, int y, int graph[][SIZE], int graph2[SIZE], int soF
     graph2[x] = 1;
     int i;
     int e;
-    int j = 50;
+    int j = INF; // Initialize with a large value
     for (i = 0; i < SIZE; i++)
     {
         if (graph[x][i] == 1 && graph2[i] == 0)
         {
-            e = shortest_pathhelp(i, y, graph, graph2, soFer + 1 < j);
-            if (e)
+            e = shortest_pathhelp(i, y, graph, graph2, soFer + 1);
+            if (e != NO_PATH && e < j)
             {
                 j = e;
             }
         }
     }
 
-    return j;
+    return (j == INF) ? NO_PATH : j; // Return NO_PATH if no valid path found
 }
-
 
 int shortest_path(int x, int y, int graph[][SIZE])
 {
     int graph2[SIZE] = {0};
     int t = shortest_pathhelp(x, y, graph, graph2, 0);
-    if (t == 50)
-    {
-        return -1;
-    }
     return t;
 }
 
 int is_there_path(int x, int y, int graph[][SIZE])
 {
     int t = shortest_path(x, y, graph);
-    if (t > -1)
-    {
-        return 1;
-    }
-
-    return 0;
+    return (t != NO_PATH);
 }
-
-
-
-
