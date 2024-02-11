c6175328fc1b8ff62a9e01459c68bae4b6366ed1
arielag1998@gmail.com <ariela@ubuntu.myguest.virtualbox.org>
2024-02-11 18:36:48 +0200

    nuu beimah

--- my_math.c
+++ my_math.c
@@ -4,62 +4,45 @@
 #ifndef SIZE
 #define SIZE 10 // size of matrix board
 #endif
-#define INF 999999
-
-int shortest_pathhelp(int x, int y, int graph[][SIZE], int graph2[SIZE], int soFer)
-{
-    if (x == y)
-    {
-        return soFer;
-    }
-    graph2[x] = 1;
-    int graph3[SIZE];
-    for (int i = 0; i < SIZE; i++)
-    {
-        graph3[i] = graph2[i];
-        /* code */
-    }
-
-    int i;
-    int e;
-    int j = INF;
-    for (i = 0; i < SIZE; i++)
-    {
-        if (graph[x][i] > 0 && graph2[i] == 0)
-        {
-            e = shortest_pathhelp(i, y, graph, graph3, soFer + graph[x][i]);
-            if (e < j)
-            {
-                j = e;
-            }
-        }
-    }
-
-    return j;
-}
 
 int is_there_path(int x, int y, int graph[][SIZE])
 {
     if (x == y)
         return -1;
     int t = shortest_path(x, y, graph);
-    if (t > -1)
-    {
-        return 1;
-    }
-
-    return 0;
+    return t;
 }
 
 int shortest_path(int x, int y, int graph[][SIZE])
 {
-    int graph2[SIZE] = {0};
-    int t = shortest_pathhelp(x, y, graph, graph2, 0);
-    if (t == INF)
+    int k, i, j;
+    int dist[SIZE][SIZE];
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            dist[i][j] = graph[i][j];
+        }
+    }
+    for (k = x; k < SIZE; k++)
+    {
+        for (i = 0; i < SIZE; i++)
+        {
+            for (j = 0; j < SIZE; j++)
+            {
+                if (dist[i][k] != 0 && dist[k][j] != 0)
+                {
+                    if (dist[i][j] > (dist[i][k] + dist[k][j]))
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    if (dist[x][y] == 0)
     {
         return -1;
     }
-    return t;
+    return dist[x][y];
 }
 
 int getData(int graph[][SIZE])
